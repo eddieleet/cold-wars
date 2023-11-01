599aee9a96fa151e8ea9445f1f726bff56584429
eddieleet <eddieleet2@gmail.com>
2023-11-01 15:19:47 +0200

    first-commit

---
+++ hash-main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+using namespace std;
+using namespace std;
+
+int main(){
+
+return 0;
+}
---
+++ hash.h
@@ -0,0 +1,14 @@
+#ifndef HASH_H
+#define HASH_H
+
+class hash{
+private:
+int tableSizes;
+
+public:
+hash();
+   int Hash(string key);
+};
+
+
+#endif /*HASH_H */
